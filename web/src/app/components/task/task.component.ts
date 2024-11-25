23fde979c54850e0f2d050aadf17c6b1b381b39b
fyambos <feliciteyambos@gmail.com>
2024-11-25 15:19:21 +0100

    feat: Task management logic

--- web/src/app/components/task/task.component.ts
+++ web/src/app/components/task/task.component.ts
@@ -6,4 +6,18 @@ import { Component } from '@angular/core';
 })
 export class TaskComponent {
 
+  tasks = [
+    { name: 'Task 1', description: 'This is the first task', completed: false },
+    { name: 'Task 2', description: 'This is the second task', completed: false },
+    { name: 'Task 3', description: 'This is the third task', completed: false }
+  ];
+
+  removeTask = (index: number) => {
+    this.tasks.splice(index, 1);
+  }
+
+  updateTask = (index: number, task: any) => {
+    this.tasks[index] = task;
+  }
+
 }
