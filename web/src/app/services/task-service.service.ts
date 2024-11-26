ae39297d115396cf87df564eaee8cc1e6544731f
Rosvadle MANFO <florientmanfo3@outlook.fr>
2024-11-26 15:03:30 +0100

    feat: Stop event propagation.

--- web/src/app/services/task-service.service.ts
+++ web/src/app/services/task-service.service.ts
@@ -1,9 +1,11 @@
-import { Injectable } from '@angular/core';
+import { Inject, Injectable } from '@angular/core';
+import { Firestore } from '@angular/fire/firestore';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskServiceService {
 
-  constructor() { }
+  constructor( private readonly firestore: Firestore) { }
+
 }
